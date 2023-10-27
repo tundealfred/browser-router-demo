df755c857f0538df8ca140508c88125c9a55e885
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2023-10-27 11:34:56 +0100

    Git gud

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <h1>React Router Dom</h1>
+          <Link to="">Home</Link>
+          <Link to="./about">About</Link>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+
+        <footer>
+          <p>&copy; Tim the Bin</p>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
